9cc9c19cd1e5d8879721e0f217efcba1c773b475
Sai <sai@SAIs-MacBook-Pro.local>
2023-08-09 18:48:47 +0530

    adding user, posts

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -38,9 +42,12 @@ const upload = multer({ storage })
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes)
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 const PORT = process.env.PORT || 6001;
